9db1d7bc75883353c5b88da8ce5ec19e526f3c73
Kennan LeJeune <kc.lejeune@gmail.com>
2019-02-01 21:59:22 -0500

    i screwed up
    Signed-off-by: Kennan LeJeune <kc.lejeune@gmail.com>

--- src/CombatAgentMod.java
+++ src/CombatAgentMod.java
@@ -1,4 +1,4 @@
-import edu.cwru.sepia.action.Action;
+import edu.cwru.sepia.action.*;
 import edu.cwru.sepia.agent.Agent;
 import edu.cwru.sepia.environment.model.history.History;
 import edu.cwru.sepia.environment.model.history.History.HistoryView;
@@ -81,27 +81,11 @@ public class CombatAgentMod extends Agent
                 towerId = enemyId;
             }
         }
-        
-        int i = 0;
-        
-        // lure out enemy footmen
-        actions.put(pFootmen.get(0), Action.createCompoundAttack(pFootmen.remove(0), towerId));
-        
-    /*
-        while (!pArchers.isEmpty())
+    
+        if (!pFootmen.isEmpty())
         {
-            // attack all footmen with archers and ballistas
-            actions.put(pArchers.get(0), Action.createCompoundAttack(pArchers.get(0), eFootmen.get(i % eFootmen.size
-            ())));
-            i++;
-            pArchers.remove(0);
-        }*/
-        while (!pArchers.isEmpty())
-        {
-            // attack all footmen with archers and ballistas
-            actions.put(pArchers.get(0), Action.createCompoundAttack(pArchers.get(0),
-                    eFootmen.get(i++ % eFootmen.size())));
-            pArchers.remove(0);
+            int id = pFootmen.remove(0);
+            actions.put(id, Action.createCompoundAttack(id, towerId));
         }
         
         return actions;
@@ -110,70 +94,78 @@ public class CombatAgentMod extends Agent
     @Override
     public Map<Integer, Action> middleStep(StateView stateView, HistoryView historyView)
     {
-        
-        /*// This is a list of enemy units
+        // This is a list of enemy units
         List<Integer> enemyUnitIDs = stateView.getUnitIds(enemyPlayerNum);
-        
-        // This stores the action that each unit will perform
-        // if there are no changes to the current actions then this
-        // map will be empty
-        Map<Integer, Action> actions = new HashMap<Integer, Action>();
-        
+    
+        // stores unit actions
+        Map<Integer, Action> actions = new HashMap<>();
+    
+        // do nothing because there is no one left to attack
         if (enemyUnitIDs.size() == 0)
         {
-            // Nothing to do because there is no one left to attack
             return actions;
         }
         
-        List<Integer> unitIDs = stateView.getUnitIds(playernum);
-        
-        UnitView unitView;
-        
-        List<Integer> footmanIDs = new ArrayList<Integer>();
-        List<Integer> archerIDs = new ArrayList<Integer>();
-        List<Integer> towerIDs = new ArrayList<Integer>();
-        List<Integer> ballistaIDs = new ArrayList<Integer>();
-        
-        //This for loop puts the units in their respective lists, so
-        //we can identify what kind of unit they are
-        for (Integer unitID : unitIDs)
+        int currentStep = stateView.getTurnNumber();
+    
+        List<Integer> eFootmen = new ArrayList<>();
+        for (Integer id : enemyUnitIDs)
         {
-            unitView = stateView.getUnit(unitID);
-            
-            String unitType = unitView.getTemplateView().getName();
-            
-            if (unitType.equals("Footman"))
+            if (stateView.getUnit(id).getTemplateView().getName().equals("Footman"))
             {
-                footmanIDs.add(unitID);
-            }
-            else if (unitType.equals("Archer"))
-            {
-                archerIDs.add(unitID);
-            }
-            else if (unitType.equals("Tower"))
-            {
-                towerIDs.add(unitID);
-            }
-            else
-            {
-                ballistaIDs.add(unitID);
+                eFootmen.add(id);
             }
         }
-        int currentStep = stateView.getTurnNumber();
-        int min = -1;
+        eFootmen.sort(Comparator.comparingInt(o -> stateView.getUnit(o).getTemplateView().getBaseHealth()));
+    
+        List<Integer> pFootmen = new ArrayList<>();
+        List<Integer> pArchers = new ArrayList<>();
+        List<Integer> pBallistas = new ArrayList<>();
+        
         //go through action history
         for (ActionResult feedback : historyView.getCommandFeedback(playernum, currentStep - 1).values())
         {
-            // if the previous action is no longer in progress (either due to failure or completion)
-            // then add a new action for this unit
+            // if the previous action is no longer in progress (either due to failure or completion), assign a new one
             if (feedback.getFeedback() != ActionFeedback.INCOMPLETE)
             {
                 int unitID = feedback.getAction().getUnitId();
-                
-                // actions.put(unitID, Action.createCompoundAttack(unitID, ));
+                String unitType = stateView.getUnit(unitID).getTemplateView().getName();
+                if (unitType.equals("Archer"))
+                {
+                    pArchers.add(unitID);
+                }
+                else if (unitType.equals("Footman"))
+                {
+                    pFootmen.add(unitID);
+                }
+                else
+                {
+                    pBallistas.add(unitID);
+                }
             }
-        }*/
-        return null;
+        }
+    
+        int i = 0;
+    
+        if (!pFootmen.isEmpty())
+        {
+            int id = pFootmen.remove(0);
+            actions.put(id, Action.createCompoundAttack(id, eFootmen.get(0)));
+        }
+    
+        while (!pBallistas.isEmpty())
+        {
+            int id = pBallistas.remove(0);
+            actions.put(id, Action.createCompoundAttack(id, eFootmen.get(0)));
+        }
+    
+        while (!pArchers.isEmpty())
+        {
+            int id = pArchers.remove(0);
+            actions.put(id, Action.createCompoundAttack(id, eFootmen.get(0)));
+        }
+    
+        return actions;
     }
     
     @Override
