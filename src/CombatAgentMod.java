f649d38b775feb2891fa0ab2bcc6ac890f42bd26
Kennan LeJeune <kc.lejeune@gmail.com>
2019-02-01 21:21:06 -0500

    lure out some bitches
    Signed-off-by: Kennan LeJeune <kc.lejeune@gmail.com>

--- src/CombatAgentMod.java
+++ src/CombatAgentMod.java
@@ -1,158 +1,185 @@
 import edu.cwru.sepia.action.Action;
-import edu.cwru.sepia.action.ActionFeedback;
-import edu.cwru.sepia.action.ActionResult;
 import edu.cwru.sepia.agent.Agent;
 import edu.cwru.sepia.environment.model.history.History;
 import edu.cwru.sepia.environment.model.history.History.HistoryView;
 import edu.cwru.sepia.environment.model.state.State;
 import edu.cwru.sepia.environment.model.state.State.StateView;
-import edu.cwru.sepia.environment.model.state.Unit;
+import edu.cwru.sepia.environment.model.state.Unit.UnitView;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class CombatAgentMod extends Agent
 {
-	
-	private int enemyPlayerNum = 1;
-	
-	public CombatAgentMod(int playernum, String[] otherargs)
-	{
-		super(playernum);
-		if(otherargs.length > 0)
-		{
-			enemyPlayerNum = new Integer(otherargs[0]);
-		}
-	}
-	
-	@Override
-	public Map<Integer, Action> initialStep(StateView newstate,
-	                                        HistoryView statehistory)
-	{
-		// This stores the action that each unit will perform
-		// if there are no changes to the current actions then this
-		// map will be empty
-		Map<Integer, Action> actions = new HashMap<Integer, Action>();
-		
-		// This is a list of all of your units
-		// Refer to the resource agent example for ways of
-		// differentiating between different unit types based on
-		// the list of IDs
-		List<Integer> myUnitIDs = newstate.getUnitIds(playernum);
-		
-		// This is a list of enemy units
-		List<Integer> enemyUnitIDs = newstate.getUnitIds(enemyPlayerNum);
-		
-		if(enemyUnitIDs.size() == 0)
-		{
-			// Nothing to do because there is no one left to attack
-			return actions;
-		}
-		
-		// start by commanding every single unit to attack an enemy unit
-		for(Integer myUnitID : myUnitIDs)
-		{
-			// Command all of my units to attack the first enemy unit in the list
-			actions.put(myUnitID, Action.createCompoundAttack(myUnitID, enemyUnitIDs.get(0)));
-		}
-		
-		return actions;
-	}
-	
-	@Override
-	public Map<Integer, Action> middleStep(State.StateView stateView, History.HistoryView historyView)
-	{
-		
-		// This is a list of enemy units
-		List<Integer> enemyUnitIDs = stateView.getUnitIds(enemyPlayerNum);
-		
-		int currentStep = stateView.getTurnNumber();
-		
-		// This stores the action that each unit will perform
-		// if there are no changes to the current actions then this
-		// map will be empty
-		Map<Integer, Action> actions = new HashMap<Integer, Action>();
-		
-		if(enemyUnitIDs.size() == 0)
-		{
-			// Nothing to do because there is no one left to attack
-			return actions;
-		}
-		
-		List<Integer> unitIDs = stateView.getUnitIds(playernum);
-		
-		Unit.UnitView unitView;
-		
-		List<Integer> footmanIDs = new ArrayList<Integer>();
-		List<Integer> archerIDs = new ArrayList<Integer>();
-		List<Integer> towerIDs = new ArrayList<Integer>();
-		List<Integer> ballistaIDs = new ArrayList<Integer>();
-		
-		//This for loop puts the units in their respective lists, so
-		//we can identify what kind of unit they are
-		for(Integer unitID : unitIDs)
-		{
-			unitView = stateView.getUnit(unitID);
-			
-			String unitTypeName = unitView.getTemplateView().getName();
-			
-			if(unitTypeName.equals("Footman"))
-			{
-				footmanIDs.add(unitID);
-			}
-			else if(unitTypeName.equals("Archer"))
-			{
-				archerIDs.add(unitID);
-			}
-			else if(unitTypeName.equals("Tower"))
-			{
-				towerIDs.add(unitID);
-			}
-			else
-			{
-				ballistaIDs.add(unitID);
-			}
-		}
-		int min = -1;
-		//go through action history
-		for(ActionResult feedback : historyView.getCommandFeedback(playernum, currentStep - 1).values())
-		{
-			// if the previous action is no longer in progress (either due to failure or completion)
-			// then add a new action for this unit
-			if(feedback.getFeedback() != ActionFeedback.INCOMPLETE)
-			{
-				int unitID = feedback.getAction().getUnitId();
-				
-				int xplayer = stateView.getUnit(unitID).getXPosition();
-				int yplayer = stateView.getUnit(unitID).getYPosition();
-				
-				int enemyMinID = -1;
-				
-				for(int i = 0; i < enemyUnitIDs.size(); i++)
-				{
-					int xenemy = stateView.getUnit(enemyUnitIDs.get(i)).getXPosition();
-					int yenemy = stateView.getUnit(enemyUnitIDs.get(i)).getYPosition();
-					int distance = (xenemy - xplayer) + (yenemy - yplayer);
-					if(distance < min)
-					{
-						min = distance;
-						enemyMinID = enemyUnitIDs.get(i);
-					}
-				}
-				actions.put(unitID, Action.createCompoundAttack(unitID, enemyMinID));
-			}
-		}
-		return actions;
-	}
-	
-	@Override
+    
+    private int enemyPlayerNum = 1;
+    
+    public CombatAgentMod(int playernum, String[] otherargs)
+    {
+        super(playernum);
+        if (otherargs.length > 0)
+        {
+            enemyPlayerNum = new Integer(otherargs[0]);
+        }
+    }
+    
+    @Override
+    public Map<Integer, Action> initialStep(StateView newstate, HistoryView statehistory)
+    {
+        // get lists of player and enemy units
+        List<Integer> unitIDs = newstate.getUnitIds(playernum);
+        List<Integer> enemyUnitIDs = newstate.getUnitIds(enemyPlayerNum);
+        
+        // stores each unit's performed actions
+        Map<Integer, Action> actions = new HashMap<>();
+        
+        if (enemyUnitIDs.size() == 0)
+        {
+            // Nothing to do because there is no one left to attack
+            return actions;
+        }
+        
+        List<Integer> pFootmen = new ArrayList<>();
+        List<Integer> pArchers = new ArrayList<>();
+        List<Integer> pBallistas = new ArrayList<>();
+        List<Integer> eFootmen = new ArrayList<>();
+        
+        UnitView unitView;
+        
+        // separate player units by type
+        for (Integer unitID : unitIDs)
+        {
+            unitView = newstate.getUnit(unitID);
+            
+            String unitType = unitView.getTemplateView().getName();
+            
+            if (unitType.equals("Footman"))
+            {
+                pFootmen.add(unitID);
+            }
+            else if (unitType.equals("Archer"))
+            {
+                pArchers.add(unitID);
+            }
+            else
+            {
+                pBallistas.add(unitID);
+            }
+        }
+        
+        int towerId = 5;
+        for (Integer enemyId : enemyUnitIDs)
+        {
+            unitView = newstate.getUnit(enemyId);
+            String unitType = unitView.getTemplateView().getName();
+            if (unitType.equals("Footman"))
+            {
+                eFootmen.add(enemyId);
+            }
+            else
+            {
+                towerId = enemyId;
+            }
+        }
+        
+        int i = 0;
+        
+        // lure out enemy footmen
+        actions.put(pFootmen.get(0), Action.createCompoundAttack(pFootmen.remove(0), towerId));
+        
+    /*
+        while (!pArchers.isEmpty())
+        {
+            // attack all footmen with archers and ballistas
+            actions.put(pArchers.get(0), Action.createCompoundAttack(pArchers.get(0), eFootmen.get(i % eFootmen.size
+            ())));
+            i++;
+            pArchers.remove(0);
+        }*/
+        while (!pArchers.isEmpty())
+        {
+            // attack all footmen with archers and ballistas
+            actions.put(pArchers.get(0), Action.createCompoundAttack(pArchers.get(0),
+                    eFootmen.get(i++ % eFootmen.size())));
+            pArchers.remove(0);
+        }
+        
+        return actions;
+    }
+    
+    @Override
+    public Map<Integer, Action> middleStep(StateView stateView, HistoryView historyView)
+    {
+        
+        /*// This is a list of enemy units
+        List<Integer> enemyUnitIDs = stateView.getUnitIds(enemyPlayerNum);
+        
+        // This stores the action that each unit will perform
+        // if there are no changes to the current actions then this
+        // map will be empty
+        Map<Integer, Action> actions = new HashMap<Integer, Action>();
+        
+        if (enemyUnitIDs.size() == 0)
+        {
+            // Nothing to do because there is no one left to attack
+            return actions;
+        }
+        
+        List<Integer> unitIDs = stateView.getUnitIds(playernum);
+        
+        UnitView unitView;
+        
+        List<Integer> footmanIDs = new ArrayList<Integer>();
+        List<Integer> archerIDs = new ArrayList<Integer>();
+        List<Integer> towerIDs = new ArrayList<Integer>();
+        List<Integer> ballistaIDs = new ArrayList<Integer>();
+        
+        //This for loop puts the units in their respective lists, so
+        //we can identify what kind of unit they are
+        for (Integer unitID : unitIDs)
+        {
+            unitView = stateView.getUnit(unitID);
+            
+            String unitType = unitView.getTemplateView().getName();
+            
+            if (unitType.equals("Footman"))
+            {
+                footmanIDs.add(unitID);
+            }
+            else if (unitType.equals("Archer"))
+            {
+                archerIDs.add(unitID);
+            }
+            else if (unitType.equals("Tower"))
+            {
+                towerIDs.add(unitID);
+            }
+            else
+            {
+                ballistaIDs.add(unitID);
+            }
+        }
+        int currentStep = stateView.getTurnNumber();
+        int min = -1;
+        //go through action history
+        for (ActionResult feedback : historyView.getCommandFeedback(playernum, currentStep - 1).values())
+        {
+            // if the previous action is no longer in progress (either due to failure or completion)
+            // then add a new action for this unit
+            if (feedback.getFeedback() != ActionFeedback.INCOMPLETE)
+            {
+                int unitID = feedback.getAction().getUnitId();
+                
+                // actions.put(unitID, Action.createCompoundAttack(unitID, ));
+            }
+        }*/
+        return null;
+    }
+    
+    @Override
     public void terminalStep(State.StateView stateView, History.HistoryView historyView)
     {
-	    System.out.println("Finished the episode");
+        System.out.println("Finished the episode");
     }
     
     @Override
